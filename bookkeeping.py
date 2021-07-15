282c507a69137322441589de0660609948478448
Li ChengYang <sinnerunoclay@gmail.com>
2021-07-15 16:24:36 +0800

    second version

--- bookkeeping.py
+++ bookkeeping.py
@@ -1,11 +1,17 @@
-# 記帳程式
+# 記帳程式，二維清單
 
 products = []
+
 while True:
 	name = input('請輸入商品名稱, 按q離開: ')
 	if name == 'q':
 		print('目前清單如下: ', products, '共計有', len(products), '項商品')
 		break
-	products.append(name.strip())
+	price = input('請輸入商品價格: ')
+	# tmp = []
+	# tmp.append(name.strip())
+	# tmp.append(price.strip())
+	# 也可以寫成這樣 tmp = [name.strip(), price.strip()]
+	products.append([name.strip(), price.strip()])
 
 
